bcd0fd409a151721d9c351ce1d0e28c0c394ba69
MikeWencel <wencel.michael@gmail.com>
2020-04-26 13:33:45 +0200

    updated writeFile

--- Introduction_of_nodeJs/first_app.js
+++ Introduction_of_nodeJs/first_app.js
@@ -1,1 +1,7 @@
-console.log("Hello Node.js");
+const fs = require("fs");
+
+fs.writeFile('hello.txt',"Hello from Node.js",function(err,result){
+    if(err) console.log('error',err)
+});
+
+console.log("Hello")
